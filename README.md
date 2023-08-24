6efe43f5209a9a648ecb70d69f6f4bd51d874901
Christoph Jean-Luc Schneider <kopilot.schneider@protonmail.com>
2023-08-24 22:42:32 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# stubit
+
+`stu`pid `bit` library.
+
+[![Crates.io](https://img.shields.io/crates/v/stubit)](https://crates.io/crates/stubit)
+[![Documentation](https://docs.rs/stubit/badge.svg)](https://docs.rs/stubit)
+
+*work in progress*
---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+//! `stu`pid `bit` library.
+//!
+//! [![Crates.io](https://img.shields.io/crates/v/stubit)](https://crates.io/crates/stubit)
+//! [![Documentation](https://docs.rs/stubit/badge.svg)](https://docs.rs/stubit)
+//! 
+//! *work in progress*
+//! 
+
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn it_works() {
+//         assert_eq!(4, 4);
+//     }
+// }
